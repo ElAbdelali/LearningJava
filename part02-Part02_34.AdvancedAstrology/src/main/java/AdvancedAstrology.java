42f39dd2e49368439eb0ad9b8609bb18cb2a3a01
Ali Eljaouhari <eljaouhariabdelali@outlook.com>
2020-09-13 17:06:14 -0500

    Completed Part two of Mooc.fi course utilizing methods to print a christmas tree

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,14 +1,14 @@
-
 public class AdvancedAstrology {
-
+ 
     public static void printStars(int number) {
         int stars = 1;
         while (stars <= number){
             System.out.print("*");
             stars++;
         }
+        System.out.println("");
     }
-
+ 
     public static void printSpaces(int number) {
         int spaces = 0;
         while (number > spaces){
@@ -16,7 +16,7 @@ public class AdvancedAstrology {
             number--;
         }
     }
-
+ 
     public static void printTriangle(int size) {
         int numOfStars = 1;
         int numOfSpaces = (size - 1);
@@ -26,21 +26,32 @@ public class AdvancedAstrology {
             printStars(numOfStars);
             numOfSpaces--;
             numOfStars++;
-            System.out.println("");
         } 
-
+ 
     }
-
+ 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        int numOfStars = 1;
+        int numOfSpaces = (height - 1);
+        for (int i = 0; i < height; i++){
+            printSpaces(numOfSpaces);
+            printStars(numOfStars);
+            numOfSpaces--;
+            numOfStars += 2;
+        }
+        int spaces =  height - 2;
+        printSpaces(spaces);
+        System.out.println("***");
+        printSpaces(spaces);
+        System.out.println("***");
     }
-
+ 
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
-
-        printTriangle(5);
+ 
+        printTriangle(3);
         System.out.println("---");
-        christmasTree(4);
+        christmasTree(3);
         System.out.println("---");
         christmasTree(10);
     }
